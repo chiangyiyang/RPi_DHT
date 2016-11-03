8ee50b3fd92cbada5c50fd87550f842661969232
yiyang.c <yiyang.c@gmail.com>
2016-11-03 08:43:39 +0000

    Add thingspeak upload and download function

--- dht11_get.py
+++ dht11_get.py
@@ -7,23 +7,31 @@ from time import sleep
 from random import randint as ri
 import Adafruit_DHT
 import json
+#import math
+
 READ_API_KRY = "XIJWWMVIG7IBQE8H"
 CHANNEL_ID = "178333"
 
 # main() function
 def main():
+  f = open('db_file', 'w+')
   while True:
     try:
-      conn = urllib2.urlopen( "http://api.thingspeak.com/channels/%s/feeds/last.json?api_key=%s" 
+      conn = urllib2.urlopen( "http://api.thingspeak.com/channels/%s/feed.json?api_key=%s" 
                            % (CHANNEL_ID, READ_API_KRY))
       response = conn.read()
       print "http status code= %s" % (conn.getcode())
       data = json.loads(response)
-      print data
+      #print data
+      for idx, feed in enumerate(data['feeds']):
+        print str(idx) + "      DH: " + feed['field1'] + ", T: " + feed['field2']
+      print "End of Record"
+      f.write(str(data))
       
       conn.close()
       sleep(10)
     except:
+      f.close()
       print "\nExiting"
       break
 
